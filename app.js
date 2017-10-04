eb66bed1e6a52ad96cdccbf2bedd78d2d82ae361
James McPherson <mcpherson.jamesd@gmail.com>
2017-10-04 10:46:51 -0700

    Added node debugger for chrome

--- app.js
+++ app.js
@@ -10,6 +10,14 @@ app.get('/', (request, response) => {
     // The 'response' object represets the reply that our
     // server is going to send to the client
 
+
+    // When running node with the '--inspect' command,
+    // you open a Chrome console and click on the Node logo in the
+    // upper-left corner of the console to opwn a dedicated
+    // debugger for Node. Anywhere you wriet the keyword, 'debugger'
+    // in your code, the debugger will pause program and you'll
+    // be able to inspect the state of your program.
+    // debugger
     response.send('Hello, World!')
 });
 
